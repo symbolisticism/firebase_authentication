f425cc1f441a6fc28000d79dc55f6e68b1f1b1c3
Gabriele Peck <peck.gabriele@gmail.com>
2023-08-31 13:02:58 -0700

    added initializeApp function from Firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,11 @@
 import 'package:authentication/screens/authentication_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MaterialApp(home: AuthenticationScreen()));
 }
