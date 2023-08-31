edda52054bfad05e550e2d942af4c38e98ac557f
Gabriele Peck <peck.gabriele@gmail.com>
2023-08-31 11:34:34 -0700

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:authentication/screens/authentication_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MaterialApp(home: AuthenticationScreen()));
+}
