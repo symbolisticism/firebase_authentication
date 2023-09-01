46c11e9dc58230e39023fc287de80f9502bffb4b
Gabriele Peck <peck.gabriele@gmail.com>
2023-09-01 14:54:13 -0700

    started writing the logic to send user information to firebase

--- lib/widgets/sign_in_form.dart
+++ lib/widgets/sign_in_form.dart
@@ -23,6 +23,7 @@ class _SignInFormState extends State<SignInForm> {
   String _enteredPassword = '';
   // boolean to switch between sign up and log in modes
   bool _isSigningIn = true;
+  bool _isAuthenticating = false;
 
   // METHODS
   void submit() async {
@@ -32,7 +33,21 @@ class _SignInFormState extends State<SignInForm> {
 
     _formKey.currentState!.save();
 
+    try {
+      setState(() {
+        _isAuthenticating = true;
+      });
 
+      if (_isSigningIn) {
+        final userCredentials = await _firebase.signInWithEmailAndPassword(
+            email: _enteredUsername, password: _enteredPassword);
+      } else {
+        final userCredentials = await _firebase.createUserWithEmailAndPassword(
+            email: _enteredUsername, password: _enteredPassword);
+      }
+
+      
+    } catch (e) {}
   }
 
   @override
@@ -93,41 +108,44 @@ class _SignInFormState extends State<SignInForm> {
             },
           ),
           const SizedBox(height: 24),
+          Column(
+            children: [
+              if (!_isAuthenticating)
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    ElevatedButton(
+                      onPressed: () {
+                        if (_formKey.currentState!.validate()) {
+                          submit();
+                        }
+                      },
+                      child: Text(_isSigningIn ? 'Log In' : 'Sign Up'),
+                    ),
+                  ],
+                ),
+              const SizedBox(height: 36),
+              // button to switch to sign up mode
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(_isSigningIn
+                      ? 'Don\'t have an account?'
+                      : 'Already have an account?'),
+                  TextButton(
+                    onPressed: () {
+                      setState(() {
+                        _isSigningIn = !_isSigningIn;
+                      });
+                    },
+                    child: Text(_isSigningIn ? 'Sign Up' : 'Log In'),
+                  ),
+                ],
+              ),
+              if (_isAuthenticating) const CircularProgressIndicator(),
+            ],
+          ),
           // button to send user info to backend, verify, and log in
-          Row(
-            mainAxisAlignment: MainAxisAlignment.end,
-            children: [
-              ElevatedButton(
-                onPressed: () {
-                  if (_formKey.currentState!.validate()) {
-                    ScaffoldMessenger.of(context).showSnackBar(
-                      const SnackBar(
-                        content: Text('Logged In!'),
-                      ),
-                    );
-                  }
-                },
-                child:
-                    Text(_isSigningIn ? 'Log In' : 'Sign Up'),
-              ),
-            ],
-          ),
-          const SizedBox(height: 36),
-          // button to switch to sign up mode
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text(_isSigningIn ? 'Don\'t have an account?' : 'Already have an account?'),
-              TextButton(
-                onPressed: () {
-                  setState(() {
-                    _isSigningIn = !_isSigningIn;
-                  });
-                },
-                child: Text(_isSigningIn ? 'Sign Up' : 'Log In'),
-              ),
-            ],
-          ),
         ],
       ),
     );
