d327e7d805e0031d8bbb0d4a8d3748896da7e3bc
Gabriele Peck <peck.gabriele@gmail.com>
2023-09-01 13:47:39 -0700

    finished layout for logging in and signing up, included form and validation

--- lib/screens/auth_screen.dart
+++ lib/screens/auth_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:authentication/widgets/sign_in_form.dart';
 
 class AuthScreen extends StatefulWidget {
   const AuthScreen({super.key});
@@ -12,8 +13,17 @@ class AuthScreen extends StatefulWidget {
 class _AuthScreenState extends State<AuthScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(appBar: AppBar(
-      title: const Text('User Authentication'),
-    ),);
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('User Authentication'),
+      ),
+      body: const Center(
+        // used Center here to get the image to center
+        child: Padding(
+          padding: EdgeInsets.all(16.0),
+          child: SignInForm(),
+        ),
+      ),
+    );
   }
 }
