436f370deb9535d774e3d45f1f4e82a759941f07
Gabriele Peck <peck.gabriele@gmail.com>
2023-09-01 10:18:30 -0700

    finally was able to configure firebase and get the app running

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:authentication/screens/auth_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:authentication/firebase_options.dart';
 
 ColorScheme scheme = ColorScheme.fromSeed(seedColor: Colors.orange.shade600);
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(
     MaterialApp(
       home: const AuthScreen(),
