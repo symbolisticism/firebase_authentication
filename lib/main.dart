9fe5f743c793c34435e7422eb4ae8183fe9153ac
Gabriele Peck <peck.gabriele@gmail.com>
2023-09-06 23:09:48 -0700

    added StreamBuilder to watch for changes in user authentication from Firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,10 @@
+import 'package:authentication/screens/home_screen.dart';
 import 'package:flutter/material.dart';
+// package imports
+import 'package:authentication/screens/splash_screen.dart';
 import 'package:authentication/screens/auth_screen.dart';
+// firebase imports
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:authentication/firebase_options.dart';
 
@@ -10,9 +15,21 @@ void main() async {
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(
     MaterialApp(
-      home: const AuthScreen(),
       debugShowCheckedModeBanner: false,
       theme: ThemeData.light(useMaterial3: true).copyWith(colorScheme: scheme),
+      home: StreamBuilder(
+          stream: FirebaseAuth.instance.authStateChanges(),
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return const SplashScreen();
+            }
+
+            if (snapshot.hasData) {
+              return const HomeScreen();
+            }
+
+            return const AuthScreen();
+          }),
     ),
   );
 }
